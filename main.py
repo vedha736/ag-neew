c8b09e700632e063652bc627f1ee685742350f1f
vedha736 <vedhanayaki.j2023@vitstudent.ac.in>
2026-04-01 22:36:12 +0530

    op

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print ("hdbhgd")
+def login(username, password):
+    if username == "admin" and password == "1234":
+        print("Login Successful")
+    else:
+        print("Invalid Credentials")
+
+login("admin", "1234")
